f9874cae444a792e69d10783b7b02d05e688a00e
LieutenantLitteral <antoine.grandjean1@univ-grenoble-alpes.fr>
2018-10-15 17:38:17 +0200

    addition founction Time.__add() corrected

--- time_repr.py
+++ time_repr.py
@@ -12,10 +12,10 @@ class Time(object):
         self.hour, self.minute, self.second = map(int, ui.split(':'))
 
     def __add__(self, ot):
-	var=Time("00:00:00")
-	var.second = (self.second+ot.second)%60 
-	var.min = (self.minute+ot.minute)%60 + (self.second+ot.second)//60
-	var.hour = (self.hour+ot.hour)%24 + (self.minute+ot.minute)//24
+        time_result=Time("00:00:00")
+        time_result.second = (self.second+ot.second)%60 
+        time_result.min = (self.minute+ot.minute)%60 + (self.second+ot.second)//60
+        time_result.hour = (self.hour+ot.hour)%24 + (self.minute+ot.minute)//24
         return var
 
     def __mul__(self, ot):
