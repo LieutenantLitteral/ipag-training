7bd2383e9b182fa3c4ca729f11a9e587fd40243a
User <user@eduroam-107122.grenet.fr>
2018-10-15 17:04:36 +0200

    raise value error

--- time_repr.py
+++ time_repr.py
@@ -5,11 +5,21 @@ try:
 except ImportError:
     pass
 
+
+
+
 class Time(object):
     
     def __init__(self, ui):
         """ui : str (user input)"""
         self.hour, self.minute, self.second = ui.split(':')
+
+        if self.hour > 24 :
+            raise ValueError("Not an appropriate hour (dumbass)")
+        if self.minute > 59 :
+            raise ValueError("Not an appropriate minute (dickhead)")
+        if self.second > 59 :
+            raise ValueError("Not an appropriate second (retarded)")
 
     def __add__(self, ot):
         return NotImplementedError
